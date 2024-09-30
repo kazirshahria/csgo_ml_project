c083315715b5cfd5f4fe1e52e22131edd1d7f676
Kazi Shahria <kazi.shahria@baruchmail.cuny.edu>
2024-09-30 01:24:02 -0400

    Updated program.py file

--- scraper/program.py
+++ scraper/program.py
@@ -8,7 +8,7 @@ data = json.load(file)
 driver = make_driver()
 
 count = 0
-for year in [2023, 2024]:
+for year in ["2023", "2024"]:
     scraped_lis = []
     urls = data[year]
     for url in tqdm(urls):
